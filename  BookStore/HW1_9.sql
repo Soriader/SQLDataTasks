eb851a4ceb136b49235fc3fef806784ff4158f5e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2026-06-15 22:19:36 +0200

    [~] Added result for HW1_9 - second query

---  BookStore/HW1_9.sql
+++  BookStore/HW1_9.sql
@@ -26,7 +26,9 @@ FROM orders_index_test
 WHERE placed_at >= (
     SELECT MAX(placed_at)
     FROM orders_index_test
-) - INTERVAL '30 days';
+) - INTERVAL '30 days'
+ORDER BY placed_at DESC
+LIMIT 100;
 --E
 SELECT *
 FROM orders_index_test
@@ -60,6 +62,8 @@ SELECT
 FROM pg_stat_statements
 ORDER BY total_exec_time DESC
 LIMIT 10
+
+--Test query 1
 
 -- 1. Remove the partial index on paid to prevent it from mixing test results.
 DROP INDEX IF EXISTS idx_orders_index_test_status_paid_partial;
@@ -101,4 +105,39 @@ WHERE status = 'shipped';
 --and the execution time dropped from approximately 9.038 ms
 --to approximately 6.056 ms. For status='paid', the index is also used.
 
+--Test query 2
+
+-- 1. Remove the partial index on paid to prevent it from mixing test results.
+DROP INDEX IF EXISTS idx_orders_index_test_status_paid_partial;
+
+- 2. Also remove any full index, if it already existed.
+DROP INDEX IF EXISTS idx_orders_index_test_status_full;
+
+- 3. Create a full index on the status column.
+CREATE INDEX idx_orders_index_test_status_full
+ON orders_index_test(placed_at DESC);
+
+- 4. Refresh the table statistics.
+ANALYZE orders_index_test;
+
+EXPLAIN (ANALYZE, BUFFERS)
+SELECT *
+FROM orders_index_test
+WHERE placed_at >= (
+    SELECT MAX(placed_at)
+    FROM orders_index_test
+) - INTERVAL '30 days'
+ORDER BY placed_at DESC
+LIMIT 100;
+
+--Problem:
+--Before optimization, PostgreSQL performed a full table scan for MAX(placed_at), a
+--second full scan for the date filter, and an additional sort on placed_at DESC.
+--
+--Optimization:
+--Added an index on placed_at DESC.
+--
+--Result:
+--The plan changed from Seq Scan + Sort to Index Only Scan + Index Scan. Execution
+--time dropped from approximately 31.6 ms to approximately 0.172 ms.
 
