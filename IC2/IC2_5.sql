ea4f4f5e30a08558efe006b698ae2ab0d42177ee
NewLavoisier <kacperjwawrzyniak@gmail.com>
2026-06-20 15:50:37 +0200

    [~] Added part of result to task IC2.5 - Saga's fallback path - compensation

--- IC2/IC2_5.sql
+++ IC2/IC2_5.sql
@@ -164,5 +164,186 @@ FROM updated_transfer;
 
 COMMIT;
 
+--simulation of the event publishing process
+
+BEGIN;
+
+UPDATE outbox_events
+SET
+    status = 'PUBLISHED',
+    published_at = CURRENT_TIMESTAMP
+WHERE status = 'NEW';
+
+COMMIT;
 
 
+--Saga's fallback path - compensation
+
+BEGIN;
+
+WITH new_transfer AS (
+	INSERT INTO transfers (
+		from_account_id ,
+		to_account_id,
+		amount,
+		status
+	)
+	VALUES (
+		1,
+		2,
+		75.00,
+		'PENDING'
+	)
+	RETURNING
+		id,
+		from_account_id ,
+		to_account_id,
+		amount
+
+)
+INSERT INTO outbox_events (
+    aggregate_type,
+    aggregate_id,
+    event_type,
+    payload,
+    status
+)
+SELECT
+    'TRANSFER',
+    id,
+    'TransferRequested',
+    jsonb_build_object(
+        'transferId', id,
+        'fromAccountId', from_account_id,
+        'toAccountId', to_account_id,
+        'amount', amount
+    ),
+    'NEW'
+FROM new_transfer;
+
+COMMIT;
+
+
+BEGIN;
+
+WITH transfer_to_debit AS (
+    SELECT
+        t.id,
+        t.from_account_id,
+        t.to_account_id,
+        t.amount
+    FROM transfers AS t
+    WHERE t.id = 3
+      AND t.status = 'PENDING'
+),
+debited_account AS (
+    UPDATE accounts AS a
+    SET balance = a.balance - t.amount
+    FROM transfer_to_debit AS t
+    WHERE a.id = t.from_account_id
+      AND a.balance >= t.amount
+    RETURNING
+        t.id AS transfer_id,
+        t.from_account_id,
+        t.to_account_id,
+        t.amount
+),
+updated_transfer AS (
+    UPDATE transfers AS tr
+    SET
+        status = 'DEBITED',
+        updated_at = CURRENT_TIMESTAMP
+    FROM debited_account AS d
+    WHERE tr.id = d.transfer_id
+    RETURNING
+        tr.id,
+        tr.from_account_id,
+        tr.to_account_id,
+        tr.amount
+)
+INSERT INTO outbox_events (
+    aggregate_type,
+    aggregate_id,
+    event_type,
+    payload,
+    status
+)
+SELECT
+    'TRANSFER',
+    id,
+    'MoneyDebited',
+    jsonb_build_object(
+        'transferId', id,
+        'fromAccountId', from_account_id,
+        'toAccountId', to_account_id,
+        'amount', amount
+    ),
+    'NEW'
+FROM updated_transfer;
+
+COMMIT;
+
+BEGIN;
+
+WITH transfer_to_refund AS (
+    SELECT
+        t.id,
+        t.from_account_id,
+        t.to_account_id,
+        t.amount
+    FROM transfers AS t
+    WHERE t.id = 3
+      AND t.status = 'DEBITED'
+),
+refunded_account AS (
+    UPDATE accounts AS a
+    SET balance = t.amount + a.balance
+    FROM transfer_to_refund AS t
+    WHERE a.id = t.from_account_id
+    RETURNING
+        t.id AS transfer_id,
+        t.from_account_id,
+        t.to_account_id,
+        t.amount
+),
+updated_transfer AS (
+    UPDATE transfers AS tr
+    SET
+        status = 'COMPENSATED',
+        updated_at = CURRENT_TIMESTAMP
+    FROM refunded_account AS r
+    WHERE tr.id = r.transfer_id
+    RETURNING
+        tr.id,
+        tr.from_account_id,
+        tr.to_account_id,
+        tr.amount
+)
+INSERT INTO outbox_events (
+    aggregate_type,
+    aggregate_id,
+    event_type,
+    payload,
+    status
+)
+SELECT
+    'TRANSFER',
+    id,
+    'MoneyRefunded',
+    jsonb_build_object(
+        'transferId', id,
+        'fromAccountId', from_account_id,
+        'toAccountId', to_account_id,
+        'amount', amount,
+        'reason', 'Credit failed'
+    ),
+    'NEW'
+FROM updated_transfer;
+
+COMMIT;
+
+UPDATE outbox_events
+SET
+    status = 'PUBLISHED',
+    published_at = CURRENT_TIMESTAMP
+WHERE status = 'NEW';
